5e74345276a6fc1241bcd8785c7dea762161623d
Kenneth Jackson <kjackson@connectionsis.com>
2019-02-05 20:46:07 -0600

    Initial Commit

---
+++ TableViewTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewTutorial
+//
+//  Created by Kenneth W Jackson on 2/5/19.
+//  Copyright © 2019 Connections Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
